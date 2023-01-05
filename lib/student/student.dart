18b1639d7fc35307d8db32b671ce8bea6fb58483
Glitch-bug <kwabenayiadom271@gmail.com>
2023-01-05 11:32:49 +0000

    student profile 0.4

--- lib/student/student.dart
+++ lib/student/student.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:e_learning/home/section.dart';
 
 class Student extends StatefulWidget{
+  int pages = 0;
+  List <Widget> tabs = [const Projects(),const  Courses(), const Following()];
   Student({super.key});
 
   @override 
@@ -85,7 +88,7 @@ class _StudentState extends State<Student>{
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 32),
-            child: Container(
+            child: SizedBox(
               width: 282,
               height: 56,
               // decoration: BoxDecoration(
@@ -96,112 +99,186 @@ class _StudentState extends State<Student>{
                 children: [
                   Container(
                     width: 282/3,
-                    decoration: const BoxDecoration(
-                      border: Border(bottom: BorderSide(width: 2.0, color: Color(0xff265AE8)), ),
+                    decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(width: 2.0, color: (widget.pages == 0)?const Color(0xff265AE8):const Color(0xffE9EFFD) ), ),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "03",
-                          style: TextStyle( 
-                            fontSize: 16
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          widget.pages = 0;
+                        });
+                      },
+
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Text(
+                              "03",
+                              style: TextStyle( 
+                                fontSize: 16
+                              )
+                            ),
+                          Text(
+                            "Projects",
+                            style: TextStyle( 
+                              fontSize: 16
+                            )
                           )
+                          ],
                         ),
-                      Text(
-                        "Projects",
-                        style: TextStyle( 
-                          fontSize: 16
-                        )
-                      )
-                      ],
+                      ),
+                    ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        widget.pages = 1;
+                      });
+                    },
+                    child: Container(
+                      width: 282/3,
+                      decoration: BoxDecoration(
+                        border: Border(bottom: BorderSide(width: 2.0, color: (widget.pages == 1)?const Color(0xff265AE8):const Color(0xffE9EFFD)), ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "04",
+                            style: TextStyle( 
+                              fontSize: 16
+                            )
+                          ), 
+                          Text(
+                            "Courses",
+                            style: TextStyle( 
+                              fontSize: 16
+                            )
+                          )
+                        ],
+                      ),
                     ),
                   ),
-    
-                  Container(
-                    width: 282/3,
-                    decoration: const BoxDecoration(
-                      border: Border(bottom: BorderSide(width: 2.0, color: Color(0xffE9EFFD)), ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "04",
-                          style: TextStyle( 
-                            fontSize: 16
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        widget.pages = 2;
+                      });
+                    },
+                    child: Container(
+                      width: 282/3,
+                      decoration: BoxDecoration(
+                        border: Border(bottom: BorderSide(width: 2.0, color: (widget.pages == 2)?const Color(0xff265AE8):const Color(0xffE9EFFD)), ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          Text(
+                            "20",
+                            style: TextStyle( 
+                              fontSize: 16
+                            )
+                          ), 
+                          Text(
+                            "Following",
+                            style: TextStyle( 
+                              fontSize: 16
+                            )
                           )
-                        ), 
-                        Text(
-                          "Courses",
-                          style: TextStyle( 
-                            fontSize: 16
-                          )
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 282/3,
-                    decoration: const BoxDecoration(
-                      border: Border(bottom: BorderSide(width: 2.0, color: Color(0xffE9EFFD)), ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "20",
-                          style: TextStyle( 
-                            fontSize: 16
-                          )
-                        ), 
-                        Text(
-                          "Following",
-                          style: TextStyle( 
-                            fontSize: 16
-                          )
-                        )
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ]
               ),
             ),
-          ),
-          Wrap(
-            spacing: 15,
-            runSpacing: 15,
-            children: [
-              Container(
-                width: 160,
-                height: 152,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(6),
-                  child: Image.asset("images/Project1.png")
-                )
-              ),
-              Container(
-                width: 160,
-                height: 152,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(6),
-                  child: Image.asset("images/Project2.png")
-                )
-              ),
-              Container(
-                width: 160,
-                height: 152,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(6),
-                  child: Image.asset("images/Project3.png")
-                )
-              )
-            ],
-          )
+          ), 
+          widget.tabs[widget.pages],
         ]
+      ),
+    );
+  }
+}
+
+
+class Projects extends StatelessWidget{
+  const Projects({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+    return Wrap(
+      spacing: 15,
+      runSpacing: 15,
+      children: [
+        SizedBox(
+          width: 160,
+          height: 152,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.asset("images/Project1.png")
+          )
+        ),
+        SizedBox(
+          width: 160,
+          height: 152,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.asset("images/Project2.png")
+          )
+        ),
+        SizedBox(
+          width: 160,
+          height: 152,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.asset("images/Project3.png")
+          )
+        )
+      ],
+    );
+  }
+}
+
+class Courses extends StatelessWidget{
+  const Courses({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20.0, 0, 20.0, 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ListItem(image: "images/s1.png", title: "Adobe Illustrator for all beginner artist", author: "Samuel Doe"),
+          ListItem(image:"images/s2.png", title: "Digital Illustration technique for procrete", author:"Sarrah Morry"),
+          ListItem(image: "images/s3.png", title: "Learn how to draw cartoon face in an easy way", author: "Sarrah Morry"),
+          ListItem(image: "images/s4.png", title: "Sketchbook essential for everyone", author: "Sarrah Morry")
+        ],
+      ),
+    );
+  }
+}
+
+class Following extends StatelessWidget{
+  const Following({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20.0, 0, 20.0, 20.0),
+      child: Column(
+        children: [
+          ListedProfile(image: "images/p1.png", username: "Samuel Jonas", handle: "jdoe", follows: false),
+          ListedProfile(image: "images/p2.png", username: "Mohammed Salah", handle: "jdoe", follows: false),
+          ListedProfile(image: "images/p3.png", username: "Afarat Jamil", handle: "jdoe", follows: false),
+          ListedProfile(image: "images/p4.png", username: "Samuel Jonas", handle: "jdoe", follows: false),
+          ListedProfile(image: "images/p5.png", username: "Samuel Jonas", handle: "jdoe", follows: false),
+          ListedProfile(image: "images/p6.png", username: "Samuel Jonas", handle: "jdoe", follows: false),
+          ListedProfile(image: "images/p7.png", username: "Samuel Jonas", handle: "jdoe", follows: false),
+          ListedProfile(image: "images/p9.png", username: "Samuel Jonas", handle: "jdoe", follows: false)
+        ],
       ),
     );
   }
